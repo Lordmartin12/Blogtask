15f478b18b4a2f69fd01bebbb7d5ee808c15c3a6
lordmartin12 <martinobinna2016@gmail.com>
2024-05-22 18:13:28 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog</title>
+    <link rel="stylesheet" href="css/app.css">
+    <script scr ="js/app.js"></script>
+</head>
+<body>
+
+    <div class="h-20 bg-teal-500 text-white">
+        <h1 class="text-2xl" > hello world</h1>
+        <p id="time-test"></p>
+</div>
+    
+</body>
+</html>
